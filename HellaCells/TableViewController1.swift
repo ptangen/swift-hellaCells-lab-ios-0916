5d2c2c7f4c01b377b30299a5d5dc6c68c0d6bad7
Paul Tangen <ptangen@ptangen.com>
2016-10-07 10:22:21 -0400

    Complete the hellacells lab.

---
+++ HellaCells/TableViewController1.swift
@@ -0,0 +1,107 @@
+//
+//  TableViewController1.swift
+//  HellaCells
+//
+//  Created by Paul Tangen on 10/7/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController1: UITableViewController {
+    
+    @IBOutlet var tableViewController1: UITableView!
+    
+    var myArray: [String] {
+    
+        var tempArray: [String] = []
+        for i in 1...100 {
+            tempArray.append(String(i))
+        }
+        return tempArray
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return myArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "prototypeCell", for: indexPath)
+
+        let itemTitle = myArray[indexPath.row]
+        cell.textLabel?.text = itemTitle // its ok to force unwrap a UI element
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetailSegue" {
+            let destinationViewController = segue.destination as! HellaDetailViewController
+            let selectedIndex = self.tableViewController1 .indexPathForSelectedRow?.row
+            
+            if let selectedIndex = selectedIndex {
+                destinationViewController.incomingDetailLabel = self.myArray[selectedIndex]
+            }
+        }
+    }
+
+}
